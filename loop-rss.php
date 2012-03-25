3ca9f515ee1a0a71b67072ee3199571302c49282
Rickard Andersson <rickard@montania.se>
2012-03-25 21:25:02 +0200

    Fixat så att ett meddelande visas om inga inlägg finns att visa

--- loop-rss.php
+++ loop-rss.php
@@ -1,38 +1,46 @@
 <?php global $ettan, $paged; ?>
 
-<?php foreach ( $ettan->get_posts( !is_search() , $paged ) as $post ) : ?>
+<?php if ( $ettan->have_posts() ) : ?>
 
-	<?php $site = $ettan->get_site($post->ID); ?>
+	<?php foreach ( $ettan->get_posts( !is_search() , $paged ) as $post ) : ?>
 
-	<article role="article" id="post-<?php echo $post->ID ?>" class="ettan <?php echo $post->class ?>">
-		<header>
+		<?php $site = $ettan->get_site($post->ID); ?>
 
-			<?php if ( PP_ettan::is_rss_post( $post ) ) : ?>
-			<div class="ettan-site-name">
-				<a href="<?php echo $site->url ?>"><?php echo $site->name ?></a>
+		<article role="article" id="post-<?php echo $post->ID ?>" class="ettan <?php echo $post->class ?>">
+			<header>
+
+				<?php if ( PP_ettan::is_rss_post( $post ) ) : ?>
+				<div class="ettan-site-name">
+					<a href="<?php echo $site->url ?>"><?php echo $site->name ?></a>
+				</div>
+				<?php endif ?>
+
+				<h1 class="entry-title">
+					<a <?php if ( PP_ettan::is_rss_post( $post ) ) : ?> target="_blank" <?php endif ?> href="<?php echo $post->permalink; ?>" rel="bookmark"><?php echo $post->title; ?></a>
+				</h1>
+
+				<?php get_template_part( 'entry', 'header' ); ?>
+
+			</header>
+			<div class="entry-content">
+				<?php echo $post->excerpt; ?>
 			</div>
-			<?php endif ?>
 
-			<h1 class="entry-title">
-				<a <?php if ( PP_ettan::is_rss_post( $post ) ) : ?> target="_blank" <?php endif ?> href="<?php echo $post->permalink; ?>" rel="bookmark"><?php echo $post->title; ?></a>
-			</h1>
+			<footer>
+				<?php get_template_part( 'entry', 'footer' ) ?>
+			</footer>
 
-			<?php get_template_part( 'entry', 'header' ); ?>
+			<?php get_template_part( 'entry', 'comments' ) ?>
 
-		</header>
-		<div class="entry-content">
-			<?php echo $post->excerpt; ?>
-		</div>
+		</article>
 
-		<footer>
-			<?php get_template_part( 'entry', 'footer' ) ?>
-		</footer>
+	<?php endforeach; ?>
 
-		<?php get_template_part( 'entry', 'comments' ) ?>
+	<?php get_template_part( 'pager' ) ?>
 
-	</article>
+<?php else : ?>
 
-<?php endforeach; ?>
+		<?php _e('Inga inlägg att visa.', 'piratpartiet') ?>
 
-<?php get_template_part( 'pager' ) ?>
+<?php endif ?>
 
