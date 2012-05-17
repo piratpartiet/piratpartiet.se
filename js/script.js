0f88f8f814d1d1011886a6edcf222f4a06b0b9bf
rickard <rickard@montania.se>
2012-05-17 21:26:37 +0200

    Mer fallbacks

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 (function($,m) {
 
+	var libs = '/wp-content/themes/piratpartiet.se/js/libs/';
+
 	// Fix z-index issue with youtube iframes
 	$('iframe').each(function() {
 		var url = $(this).attr('src');
@@ -9,7 +11,7 @@
 	// Polyfill for older browsers without placeholder support
 	m.load({
 		test: m.input.placeholder,
-		nope: '/wp-content/themes/piratpartiet.se/js/libs/jquery.placeholder.min.js',
+		nope: libs + 'jquery.placeholder.min.js',
 		callback: function() {
 			$("input[placeholder]").placeholder();
 		}
@@ -17,10 +19,7 @@
 
 	m.load({
 		test: $("html").hasClass('ie8') || $("html").hasClass('ie7'),
-		yep: [ '/wp-content/themes/piratpartiet.se/js/libs/nwmatcher-1.2.5.js', '/wp-content/themes/piratpartiet.se/js/libs/selectivizr-min.js'],
-		callback: function() {
-			alert('ie 7-8');
-		}
+		yep: [ libs + 'nwmatcher-1.2.5.js', libs + 'selectivizr-min.js']
 	});
 
 })(jQuery, Modernizr);
