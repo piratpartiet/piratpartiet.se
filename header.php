2fc08e537bd3a55547560f4271019bd52e9fa247
rickard <rickard@montania.se>
2012-05-30 19:40:37 +0200

    Radavstånd

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-		<meta name="description" content="">
+		<meta name="description" content="<?php bloginfo("description") ?>">
 		<meta name="author" content="Piratpartiet">
   		<meta name="viewport" content="width=device-width,initial-scale=1">
 		
